11b1efd3978797216004049461f95f6f913d0d14
ADMIN <ADMIN@LAB1-MAY32>
2023-09-04 14:31:11 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cshap_bai2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("nhap ten cua ban: ");
+            string ten = Console.ReadLine();   
+            Console.WriteLine("hello {0}!",ten);
+            Console.ReadKey();
+        }
+    }
+}
